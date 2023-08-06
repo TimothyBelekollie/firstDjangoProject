94945c4535bbbfd4e1df4021d2cccadfdcbe1240
TimothyBelekollie <belekollietimothy2@gmail.com>
2023-08-06 22:24:47 +0200

    testing

--- myvenv/pages/urls.py
+++ myvenv/pages/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import homePageView
+#from .views import homePageView
+from .views import HomePageView, AboutusView
 
 # I am going to define my url patterns
 
 urlpatterns = [
-   path('',homePageView,name='home') 
+  # path('',homePageView,name='home') 
+  path('',HomePageView.as_view(), name='home'),
+  path('aboutus/',AboutusView.as_view(), name='aboutus'),
 ]
